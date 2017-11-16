621bdac3d8149e55fe9fb8582df460cce2ec272d
MohitsBranmac <ms24546@gmail.com>
2017-11-16 19:37:57 +0530

    Adds Landing Page UI

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public todos = []
+
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
 
   }
 
+  openTodoAlert () {
+    let addTodoAlert = this.alertCtrl.create ({
+      title: "Add a Todo",
+      message: "Enter your todo",
+      inputs: [
+        {
+          type: "text",
+          name: "addTodoInput"
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+        },
+        {
+          text: "Add Todo",
+          handler: inputData => {
+            let todoText;
+            todoText = inputData.addTodoInput;
+            this.todos.push (todoText);
+          }
+        }
+      ]
+    })
+    addTodoAlert.present()
+  }
+  
 }
